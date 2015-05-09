ff9091347f7a411e9602fe517d65c0a54675436f
mlanneponders <mlyonadi@gmail.com>
2015-05-09 19:16:07 -0300

    Entrega TP-4

---
+++ Promediar2.c
@@ -0,0 +1,18 @@
+/*Informa la longitud promedio de las líneas
+* Lanneponders Yonadi,Matías
+* 09/05/2015
+*/
+
+#include <stdio.h> /* getchar EOF feof perror printf */
+#include <stdlib.h> /* EXIT_SUCCESS */
+
+int main(void) {
+int nl; /* la cantidad de lineas */
+int nc; /* la cantidad de caracteres */
+int c; /* el caracter leido */
+for(nl = 0, nc = 0; ( c = getchar()) != EOF;)
+    c=='\n'?++nl:++nc;
+!feof(stdin)?perror("No se pudo seguir leyendo de la entrada debido a un error"):NULL;
+printf("Longitud promedio: %.1f\n", nc / (float)nl );
+return EXIT_SUCCESS;
+}
